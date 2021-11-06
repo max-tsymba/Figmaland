561e514881a2005fb51cedb2f0c78af0f1573a0d
max-tsymba <young.bezero@gmail.com>
2021-11-06 18:32:54 +0200

    tailwind config

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -6,6 +6,19 @@ module.exports = {
         colors: {
             'current': 'currentColor',
             'transparent': 'transparent',
+            'white': {
+                DEFAULT: 'white',
+                '100': '#F4F4F4',
+            },
+            'black' : {
+                DEFAULT: 'black',
+                '100': '#18171D',
+            },
+            'primary-100': '#2091F9',
+            'secondary-100': '#252B42',
+            'text': '#252B42',
+            'text-second': '#374754',
+            'border-100': '#D8D8D8',
         },
         screens: {
             sm: '576px',
@@ -17,20 +30,34 @@ module.exports = {
             center: true,
             padding: '15px',
             screens: {
-              'desktop': '1300px',
+              'desktop': '1270px',
             },
         },
         fontSize: {
-
+            'sm': '15px',
+            'sm-m': '16px',
+            'm': '18px',
+            'md': '20px',
+            'lg': '28px',
+            'xl': '48px',
+            'xxl': '74px'
         },
         lineHeight: {
-
+            '10': '1',
+            '11': '1.1',
+            '12': '1.2',
+            '14': '1.4',
+            '15': '1.5',
+            '18': '1.8',
         },
         borderRadius: {
-
+            '10': '10px',
+            '35': '35px',
+            '40': '40px',
+            '48': '48px',
         },
         fontFamily: {
-            default: ['sans-serif'],
+            default: ['Roboto', 'sans-serif'],
         },
         spacing: {
 
