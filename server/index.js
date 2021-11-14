6bb223ed289f8855cb35c42b00561879cda743e1
max-tsymba <young.bezero@gmail.com>
2021-11-14 18:09:53 +0200

    set default server configs

---
+++ server/index.js
@@ -0,0 +1,24 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+const sequelize = require('./db');
+
+const PORT = process.env.PORT | 5000;
+const app = express();
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors());
+
+const start = async () => {
+    try {
+        await sequelize.authenticate()
+        await sequelize.sync()
+        app.listen(PORT, () => console.log(`Server started on ${PORT}`));
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+start();
