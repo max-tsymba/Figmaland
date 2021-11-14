00a06af963c77b83f79384ae48eeeaf3a501a633
max-tsymba <young.bezero@gmail.com>
2021-11-15 01:51:32 +0200

    registration controller + services

---
+++ server/service/user-service.js
@@ -0,0 +1,32 @@
+const UserModel = require('../models/user.model');
+const bcrypt = require('bcrypt');
+const uuid = require('uuid');
+const mailService = require('../service/mail-service');
+const tokenService = require('../service/token-service');
+const UserDto = require('../dtos/user.dto');
+
+class UserService {
+    async registration(email, password) {
+        const candidate = await UserModel.findOne({email});
+
+        if(candidate) {
+            throw new Error(`User with this email ${email} already exist!`);
+        }
+        const hashPassword = await bcrypt.hash(password, 3);
+        const activationLink = uuid.v4();
+        const user = await UserModel.create({email, password: hashPassword, activationLink});
+
+        await mailService.sendActivationMail(email, activationLink);
+
+        const userDto = new UserDto(user);  // email, id, isActivated
+        const tokens = tokenService.generateToken({...userDto});
+        await tokenService.saveToken(userDto.id, tokens.refreshToken);
+
+        return {
+            ...tokens,
+            user: userDto
+        }
+    }
+}
+
+module.exports = new UserService();
