10b48c1d083de59bc134a7a47197fb6384caeae3
max-tsymba <young.bezero@gmail.com>
2021-11-15 01:04:17 +0200

    models

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const router = require('./router/index');
+const mongoose = require('mongoose');
 
 const PORT = process.env.PORT | 5000;
 const app = express();
@@ -14,6 +15,10 @@ app.use('/api', router);
 
 const start = async () => {
     try {
+        await mongoose.connect(process.env.DB_URL, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        })
         app.listen(PORT, () => console.log(`Server started on ${PORT}`));
     } catch (e) {
         console.log(e);
